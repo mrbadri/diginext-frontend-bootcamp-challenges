fb3ddf1161eae174f26a63893db52a6be9da3eb0
mrbadri <mrbadri.dev@gmail.com>
2024-08-12 23:26:12 +0330

    feat: develop deepclone method

--- javascript/deep-clone.js
+++ javascript/deep-clone.js
@@ -7,5 +7,27 @@
  * @returns An Literal exact copy of the arg
  */
 function deepClone(arg) {
-  // TODO: Implement here
+  if (typeof arg !== "object" || arg === null) return arg;
+
+  if (Array.isArray(arg)) {
+    const copy = arg.map((item) => deepClone(item));
+    return copy;
+  }
+
+  if (typeof arg === "object") {
+    const copy = {};
+
+    for (key in arg) {
+      copy[key] = deepClone(arg[key]);
+    }
+
+    return copy;
+  }
 }
+const arg = {
+  a: { b: "test", l: 12 },
+  c: { b: "test" },
+  f: [1, 2, "3s", { h: { k: 12121 } }],
+};
+
+console.log(deepClone(arg));
